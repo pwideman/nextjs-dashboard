f556a7da60171c9733cdb2400aa0e2bfd75c80fe
Paul Wideman <pwideman@github.com>
2024-04-19 13:47:23 -0500

    chapter 14 - create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,24 +7,46 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['paid', 'pending']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce.number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['paid', 'pending'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    try {
-        const { customerId, amount, status } = CreateInvoice.parse({
-            customerId: formData.get('customerId'),
-            amount: formData.get('amount'),
-            status: formData.get('status'),
-        });
-        const amountInCents = amount * 100;
-        const date = new Date().toISOString().split('T')[0];
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
 
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    console.log(validatedFields);
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing fields. Failed to create invoice.',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
         await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
@@ -35,6 +57,7 @@ export async function createInvoice(formData: FormData) {
             message: 'Database Error: Failed to Create Invoice',
         };
     }
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
